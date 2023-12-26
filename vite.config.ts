af5579858b334ff781645753c8bb4049e5aef7e5
LxingA <kmunizh@lxinga.dev>
2023-12-25 21:35:33 -0600

    Definición del Puerto Local
    Se define mediante el readFileSync la lectura del archivo port.sc local para la definición del puerto escucha de la aplicación

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,16 @@
 @date 14/11/23 16:15
 */
 import {defineConfig} from 'vite';
+import {readFileSync} from 'fs';
+import Directory from 'path';
 import React from '@vitejs/plugin-react';
+/** Obtención del Puerto Local para el Escucha del Entorno de Desarrollo de la Aplicación */
+const local = readFileSync(Directory["join"](__dirname,"/port.sc"));
 
 export default defineConfig({
     plugins: [React()],
     server: {
-        port: 4710,
+        port: Number(local["toString"]("utf8")),
         strictPort: true
     },
     build: {
